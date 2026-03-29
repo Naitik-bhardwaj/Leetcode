f213114d36904134c80eff245067d95978002460
Naitik Bhardwaj <bhardwajnaitik88@gmail.com>
2026-03-29 09:48:24 +0530

    Time: 1 ms (99.95%), Space: 77.2 MB (59.2%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -3,7 +3,7 @@ class Solution {
         int cs = nums[0];
         int ms = nums[0];
         for(int i=1;i<nums.length;i++){
-            if(cs < 0) cs = 0;
+            //if(cs < 0) cs = 0;
             cs = Math.max(nums[i], cs+nums[i]);
             ms = Math.max(cs, ms);
         }
