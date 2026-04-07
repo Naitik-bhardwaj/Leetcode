5ba4e85c5017a13d39bcc8008e967c45c41deb7c
Naitik Bhardwaj <bhardwajnaitik88@gmail.com>
2026-04-07 09:40:32 +0530

    Time: 4 ms (41.18%), Space: 43.7 MB (8.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' ||s.charAt(i) == '['){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()) return false;
+                char top = st.pop();
+                // character closing aa gya but top pe opening he hi nhi ...
+                
+                if(s.charAt(i) == ')' && top!='(' ||
+                s.charAt(i) == '}' && top != '{' ||
+                s.charAt(i) == ']' && top!= '['){
+                    return false;
+                }
+            }
+            
+        }
+        return st.isEmpty();
+        
+    }
+}
