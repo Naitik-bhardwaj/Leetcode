99a5fea2c6023ca59546bf0fe8386f5956b6f960
Naitik Bhardwaj <bhardwajnaitik88@gmail.com>
2026-04-17 13:52:50 +0530

    Time: 134 ms (6.09%), Space: 46.4 MB (90.16%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[n];
+        solve(nums,visited,0, list);
+        return ans;
+    }
+    public void solve(int[] nums, boolean[] visited, int idx, List<Integer> list){
+        if(list.size() == nums.length) {
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(i>0 && nums[i] == nums[i-1] && !visited[i-1]){
+                continue;
+            }
+            if(visited[i] == true){
+                continue;
+            }
+            list.add(nums[i]);
+            visited[i] = true;
+            solve(nums, visited, idx+1, list);
+            list.remove(list.size()-1);
+            visited[i]  = false;
+        }
+    }
+}
