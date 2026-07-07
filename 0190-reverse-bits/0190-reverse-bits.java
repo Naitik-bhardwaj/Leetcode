e46cc0b277cebb9555d9d605eead10bc09f1e5c2
Naitik Bhardwaj <bhardwajnaitik88@gmail.com>
2026-07-07 13:42:38 +0530

    Time: 1 ms (61.22%), Space: 42.5 MB (34.62%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int s = 0;
+        for(int i=0;i<32;i++){
+            s = s << 1;
+            s = s | (n&1);
+            n = n >> 1;
+        }
+        
+        return s;
+    }
+    
+}
