df93fbbe0fd196ef840644eb0e7c02eee1d0a81d
Naitik Bhardwaj <bhardwajnaitik88@gmail.com>
2026-08-23 20:59:44 +0530

    Time: 8 ms (53.28%), Space: 47.2 MB (26.28%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.java
@@ -0,0 +1,36 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int leftKnownSum = 0;
+        int rightKnownSum = 0;
+        int leftQnMarkCount = 0;
+        int rightQnMarkCount = 0;
+        for(int i=0;i<n;i++){
+            char ch = num.charAt(i);
+            if(ch == '?'){
+                if(i < n/2){
+                    leftQnMarkCount++;
+                } else{
+                    rightQnMarkCount++;
+                }
+            } else{
+                if(i < n/2){
+                    leftKnownSum += ch-'0';
+                } else{
+                    rightKnownSum += ch-'0';
+                }
+            }
+        }
+        int totalQnMarks = leftQnMarkCount + rightQnMarkCount;
+        if(totalQnMarks % 2 == 1){
+            return true;
+        }
+        int left = 2 * leftKnownSum + 9 * leftQnMarkCount;
+        int right = 2 * rightKnownSum + 9 * rightQnMarkCount;
+        if(left == right){
+            return false;
+        }
+        return true;
+
+    }
+}
